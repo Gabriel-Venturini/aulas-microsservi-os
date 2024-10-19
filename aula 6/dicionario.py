e8e43537da8b1ab173485559bb8c0c008c83302a
Gabriel Venturini <dev.venturini@gmail.com>
2024-10-19 19:10:33 -0300

    json da ap 1.1

---
+++ aula 6/dicionario.py
@@ -0,0 +1,34 @@
+resposta = {
+    "Search": [
+        {
+            "Title": "Doctor Strange",
+            "Year": "2016",
+            "imdbID": "tt1211837",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/TE@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Strange Wilderness",
+            "Year": "2008",
+            "imdbID": "tt0489282",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MQ@@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Strange Brew",
+            "Year": "1983",
+            "imdbID": "tt0086373",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/A30A@@._V1_SX300.jpg"
+        },
+        {
+            "Title": "The Strange Love of Martha Ivers",
+            "Year": "1946",
+            "imdbID": "tt0038988",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MzNjA@._V1_SX300.jpg"
+        },
+    ],
+    "totalResults": "761",
+    "Response": "True"
+}
